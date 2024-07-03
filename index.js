099399c3b6c0e18a3b549d69ad824f51d93a0502
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-07-03 11:45:23 -0700

    Add files via upload
    simple-calculator using by inquirer

---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+//shabang
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumer" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "subtraction", "multiplication", "Devision"],
+    },
+]);
+//conditional statement:
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumer + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumer - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumer * answer.secondNumber);
+}
+else if (answer.operator === "Devision") {
+    console.log(answer.firstNumer / answer.secondNumber);
+}
+else {
+    console.log("please select the valid number");
+}
